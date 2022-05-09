0ed0900317bebdc9efff146519305a71dc739c43
游承禄 <boyyoucr@outlook.com>
2022-05-09 23:12:13 +0800

    首次提交代码

---
+++ src/main/java/com/test/TestNexus.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import com.test.nexus.Nexus;
+/**
+ * 2022/5/1 21:23
+ *
+ * @author ycl
+ */
+public class TestNexus {
+
+    public static void main(String[] args) {
+        Nexus nexus = new Nexus();
+        System.out.println(nexus.test("测试数据!"));
+    }
+
+}
